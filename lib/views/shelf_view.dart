faa7daef9535cc9b2ede22455d31e4bff81bbd21
JonathanBY15 <bariscanyaprak@gmail.com>
2023-04-20 18:43:45 +0300

    Added ListView to Shelf
    A ListView was added to the shelf. 8 book images were added to the 'images' folder. An error will occur if the 'itemCount' is set to more than the number of books. And each added images name should have the format 'book_<bookNum>.png'.

--- lib/views/shelf_view.dart
+++ lib/views/shelf_view.dart
@@ -8,12 +8,45 @@ class ShelfView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: PixelShelfAppBar(title: 'Shelf'),
-      ),
-      drawer: const PixelShelfDrawer(),
-      body: Container(color: Colors.green.shade50),
-    );
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: PixelShelfAppBar(title: 'Shelf'),
+        ),
+        drawer: const PixelShelfDrawer(),
+        body: ListView.separated(
+          // make each item closer together
+          padding: const EdgeInsets.all(5),
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return SizedBox(
+              height: 170,
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/book_$index.png',
+                    height: 170,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 0),
+                  Expanded(
+                    child: SizedBox(
+                      height: 170,
+                      child: ListTile(
+                        tileColor: Colors.green.shade100,
+                        title: Text('Book $index'),
+                        subtitle: Text('Author $index'),
+                        trailing: const Icon(Icons.more_vert),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+        ));
   }
 }
