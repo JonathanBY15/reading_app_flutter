6d58a84a377a74c1be13c87e14b6b88847725025
JonathanBY15 <bariscanyaprak@gmail.com>
2023-04-20 17:39:45 +0300

    Refactored The PixelShelfAppBar
    Each view had the entire code of the AppBar. An AppBar widget named 'PixelShelfAppBar' was created in the 'widgets' folder. The 'PixelShelfAppBar' takes the string 'title' as an input. Each view now calls the 'PixelShelfAppBar' widget with the title of the view as the input.

--- lib/views/favorites_view.dart
+++ lib/views/favorites_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reading_app_flutter/views/widgets/app_bar.dart';
 import 'package:reading_app_flutter/views/widgets/drawer.dart';
 
 class FavoritesView extends StatelessWidget {
@@ -7,57 +8,9 @@ class FavoritesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // change the color of the text and icons in the app bar
-        foregroundColor: Colors.green.shade50,
-        title: const Text('Favorites'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              // open a search bar
-            },
-          ),
-          PopupMenuButton(
-            color: Colors.green.shade50,
-            icon: const Icon(Icons.sort),
-            onSelected: (value) {
-              // Do something when a menu item is selected
-            },
-            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-              const PopupMenuItem(
-                value: 'nameAZ',
-                child: Text('Name (A-Z)'),
-              ),
-              const PopupMenuItem(
-                value: 'nameZA',
-                child: Text('Name (Z-A)'),
-              ),
-              const PopupMenuItem(
-                value: 'pagesHigh',
-                child: Text('Pages (High)'),
-              ),
-              const PopupMenuItem(
-                value: 'pagesLow',
-                child: Text('Pages (Low)'),
-              ),
-              const PopupMenuItem(
-                value: 'completionHigh',
-                child: Text('Completion (High)'),
-              ),
-              const PopupMenuItem(
-                value: 'completionLow',
-                child: Text('Completion (Low)'),
-              ),
-            ],
-          ),
-          IconButton(
-            icon: const Icon(Icons.filter_alt_outlined),
-            onPressed: () {
-              // open a search bar
-            },
-          ),
-        ],
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: PixelShelfAppBar(title: 'Favorites'),
       ),
       drawer: const DrawerMenu(),
       body: Container(color: Colors.green.shade50),
---
+++ lib/views/widgets/app_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class PixelShelfAppBar extends StatelessWidget {
+  final String title;
+
+  const PixelShelfAppBar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      foregroundColor: Colors.green.shade50,
+      title: Text(title),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {
+            // open a search bar
+          },
+        ),
+        PopupMenuButton(
+          color: Colors.green.shade50,
+          icon: const Icon(Icons.sort),
+          onSelected: (value) {
+            // Do something when a menu item is selected
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+            const PopupMenuItem(
+              value: 'nameAZ',
+              child: Text('Name (A-Z)'),
+            ),
+            const PopupMenuItem(
+              value: 'nameZA',
+              child: Text('Name (Z-A)'),
+            ),
+            const PopupMenuItem(
+              value: 'pagesHigh',
+              child: Text('Pages (High)'),
+            ),
+            const PopupMenuItem(
+              value: 'pagesLow',
+              child: Text('Pages (Low)'),
+            ),
+            const PopupMenuItem(
+              value: 'completionHigh',
+              child: Text('Completion (High)'),
+            ),
+            const PopupMenuItem(
+              value: 'completionLow',
+              child: Text('Completion (Low)'),
+            ),
+          ],
+        ),
+        IconButton(
+          icon: const Icon(Icons.filter_alt_outlined),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
